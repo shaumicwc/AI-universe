b07832ed8408c0192d0f1e8af78341b6dc360934
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-03-03 05:57:45 +0600

    code updated

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-// fetch data function
-const loadData = async (dataLimit) => {
+let dataArray = [];
+let dataLimit = 6;
+const loadData = async () =>{
     const url = 'https://openapi.programming-hero.com/api/ai/tools/'
     const res = await fetch(url);
     const data = await res.json();
-    showData(data, dataLimit);
+    dataArray = data.data.tools;
+    showData();
 }
 
-
 // show data function
-const showData = (data, dataLimit) => {
+const showData = () =>{
     const card = document.getElementById('card');
     const seeMoreBtn = document.getElementById('see-more-btn');
-    let dataArray = data.data.tools;
     card.innerHTML = '';
-
-    if (dataLimit && dataArray.length > 6) {
-        dataArray = dataArray.slice(0, 6);
-        seeMoreBtn.classList.remove('d-none');
-    }
-    else {
-        seeMoreBtn.classList.add('d-none');
-    }
-    dataArray.forEach(singleData => {
+    let dataToShow = dataArray.slice(0, dataLimit);
+    dataToShow.forEach(singleData => {
         const singleDataDiv = document.createElement('div');
         singleDataDiv.classList.add('col');
         singleDataDiv.innerHTML = `
         <div class="card">
-            <img src=${singleData.image} class="card-img-top " style="height:200px;" alt="...">
+            <img src=${singleData.image} class="card-img-top" style="height:200px;" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
                 <ol>
@@ -36,7 +29,7 @@ const showData = (data, dataLimit) => {
                 </ol>
             </div>
                 <hr>
-                <div class="d-flex" style="flex-direction:column; width: 100%;">
+            <div class="d-flex" style="flex-direction:column; width: 100%;">
                 <h5 class="mx-3">${singleData.name}</h5>
                 <div class="d-flex mx-3 justify-content-between" style="flex-direction:row;">
                 <div class="d-flex style="flex-direction:row;">
@@ -51,25 +44,30 @@ const showData = (data, dataLimit) => {
         </div>
         `
         card.appendChild(singleDataDiv);
-
-        console.log(singleData)
     });
-
+    // show/hide see-more-btn
+    if(dataLimit >= dataArray.length){
+        seeMoreBtn.classList.add('d-none');
+    }else{
+        seeMoreBtn.classList.remove('d-none');
+    }
     // spinner function called
     toggleSpinner(false);
 }
 
+toggleSpinner(true);
 
-// show limited data function
-const processSearch = (dataLimit) => {
-    toggleSpinner(true);
-    loadData(dataLimit);
-}
+const sortByDate = document.getElementById('sort-btn');
+sortByDate.addEventListener('click', function(){
+    dataArray.sort((a, b) => new Date(b.published_in) - new Date(a.published_in));
+    dataLimit = 6;
+    showData();
+});
 
-processSearch(6);
-
-document.getElementById('see-more-btn').addEventListener('click', function () {
-    processSearch()
+const seeMoreBtn = document.getElementById('see-more-btn');
+seeMoreBtn.addEventListener('click', function(){
+    dataLimit = dataArray.length;
+    showData();
 });
 
 
@@ -120,9 +118,9 @@ const showDataDetails = data =>{
     <div>
     <p class="fw-bold mt-3">Integrations:</p>
     <ul>
-        <li>${data.data.integrations && data.data.integrations[0] ? data.data.integrations[0]: "No data"}</li>
-        <li>${data.data.integrations && data.data.integrations[1] ? data.data.integrations[1]: "No data"}</li>
-        <li>${data.data.integrations && data.data.integrations[2] ? data.data.integrations[2]: "No data"}</li>
+        <li>${data.data.integrations && data.data.integrations[0] ? data.data.integrations[0]: "No data Found"}</li>
+        <li>${data.data.integrations && data.data.integrations[1] ? data.data.integrations[1]: "No data Found"}</li>
+        <li>${data.data.integrations && data.data.integrations[2] ? data.data.integrations[2]: "No data Found"}</li>
     </ul>
     </div>
     </div>
